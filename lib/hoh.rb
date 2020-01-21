d5fff4308d7c25fd3989aec332a06f2b7b049cdc
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-21 22:04:17 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,5 +44,5 @@ def naming_system
 naming_system = PORTION_1
 naming_system[:sub_category][:sub_category][:sub_category] = PORTION_2
 naming_system[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
-naming_system
+naming_system[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
 end
