2dbdde53450d0fff7fa7cf9c9aa83a494b389028
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-21 21:41:39 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -6,12 +6,8 @@
 #
  PORTION_1 = {
    label: "Kingdom",
-   sub_category: {
-     label: "Phylum",
-     sub_category: {
-       label: "Class"
-     }
-   }
+   sub_category: {label: "Phylum"},
+    sub_category: {label: "Class"}
  }
 
  PORTION_2 = {
